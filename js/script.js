98e3d12e75ba0d88bc7420bef6fc4002fa59b0ed
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-06-22 14:06:35 +0000

    Added text array at script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Set Up Array
+const text = [
+    `Pasta fish bacon peas potatoes rice green beans pork chops beef mince.  Pineapple slices in juice, cheesecake in toffee flavour, chocolate cake, caramel slices, apricots.`,
+    `Bromley Croydon Lewisham Peterborough Lakenheath Birmingham York.  Cambridge, Manea, March, Wisbech, Whittlesford, Haddenham, Ely.  Paris, Munich, Prague, Reims, Nice, Cannes, Menton, Colmar, Mulhouse, Nerja, Benidorm.`,
+    `Python JavaScript HTML5 CSS3 Bootstrap.  Django, Flask, jQuery, Font Awesome, Roboto, PDF.`,
+    ``
+];
