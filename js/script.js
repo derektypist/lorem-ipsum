d72214bb1b87d12010d5c9a07c1c5a2bbd78deb9
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-06-22 14:35:35 +0000

    Updated event listener at script.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,8 @@ form.addEventListener("submit",function(e) {
     const value = parseInt(amount.value);
     const random = Math.floor(Math.random() * text.length);
 
-    if (isNaN(value) || value<0 || value>9) {
+    // Check if the value is a number or between 0 and 9
+    if (isNaN(value) || value<=0 || value>9) {
         result.innerHTML = `<p class="result">${text[random]}</p>`;
     } else {
         let tempText = text.slice(0,value);
