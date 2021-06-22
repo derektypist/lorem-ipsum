c526d5d218dda13976c65dd4130d0b9d1f1abb25
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-06-22 14:17:50 +0000

    Completed Lorem Text at script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-// Set Up Array
+// Lorem Text
 const text = [
     `Pasta fish bacon peas potatoes rice green beans pork chops beef mince.  Pineapple slices in juice, cheesecake in toffee flavour, chocolate cake, caramel slices, apricots.`,
-    `Bromley Croydon Lewisham Peterborough Lakenheath Birmingham York.  Cambridge, Manea, March, Wisbech, Whittlesford, Haddenham, Ely.  Paris, Munich, Prague, Reims, Nice, Cannes, Menton, Colmar, Mulhouse, Nerja, Benidorm.`,
-    `Python JavaScript HTML5 CSS3 Bootstrap.  Django, Flask, jQuery, Font Awesome, Roboto, PDF.`,
-    ``
+    `Bromley Croydon Lewisham Peterborough Lakenheath Birmingham York.  Cambridge Manea March Wisbech Whittlesford Haddenham Ely Chatteris Cottenham.  Paris Munich Prague Reims Nice Cannes Menton Colmar Mulhouse Nerja Torremolinos Benalmadena Barcelona Benidorm.`,
+    `Python JavaScript HTML5 CSS3 Bootstrap.  Django Flask jQuery Font Awesome Roboto PDF.`,
+    `Black blue green red orange white yellow pink grey purple silver gold.  Emerald mint teal.`,
+    `Library school supermarket church museum sports hall cathedral tower wharf cafe house mansion.`,
+    `Crossroads Rainbow Neighbours Dallas Eastenders Pipkins Sesame Street Topcat Playschool.  The Big Match Sons and Daughters Home and Away Henry's Cat`,
+    `Metre Kilogram Second Ampere Kelvin Mole Candela.  Hertz Pascal Newton Joule Watt Henry Tesla.`,
+    `Physics Chemistry Maths English French History Geography Economics Computer Studies Games RE Algebra Statistics.  C Fortran Prolog Pascal GLIM Minitab Genstat.`,
+    `Ruth Langsford Anna Richardson Kathy Lette Kate Garraway Kay Burley Kimberley Guilfoyle Joanna Gosling Susanna Reid Marlene Lufen Rosana Franco Victoria Beckham.  Daphne Pink Roxette.`
 ];
