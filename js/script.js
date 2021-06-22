e3e932cc7fe1ddf1f84711d86ca883f2e93d8472
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-06-22 14:32:53 +0000

    Updated event listener at script.js

--- js/script.js
+++ js/script.js
@@ -11,18 +11,24 @@ const text = [
     `Ruth Langsford Anna Richardson Kathy Lette Kate Garraway Kay Burley Kimberley Guilfoyle Joanna Gosling Susanna Reid Marlene Lufen Rosana Franco Victoria Beckham.  Daphne Pink Roxette.`
 ];
 
+// Set Up Query Selectors and Get Element by Id
 const form = document.querySelector('.lorem-form');
 const amount = document.getElementById("amount");
 const result = document.querySelector('.lorem-text');
 
+// Set Up Event Listener
 form.addEventListener("submit",function(e) {
     e.preventDefault();
     const value = parseInt(amount.value);
     const random = Math.floor(Math.random() * text.length);
 
     if (isNaN(value) || value<0 || value>9) {
-        result.innerHTML = `<p class="result">${text[0]}</p>`;
+        result.innerHTML = `<p class="result">${text[random]}</p>`;
     } else {
         let tempText = text.slice(0,value);
+        tempText = tempText.map(function(item) {
+            return `<p class="result">${item}</p>`
+        }).join("");
+        result.innerHTML = tempText;
     }
 });
