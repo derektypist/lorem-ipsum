be67e7f0ae48b197731013bd83f0c87a5e71f737
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-06-22 14:22:26 +0000

    Added event listener at script.js

--- js/script.js
+++ js/script.js
@@ -5,8 +5,17 @@ const text = [
     `Python JavaScript HTML5 CSS3 Bootstrap.  Django Flask jQuery Font Awesome Roboto PDF.`,
     `Black blue green red orange white yellow pink grey purple silver gold.  Emerald mint teal.`,
     `Library school supermarket church museum sports hall cathedral tower wharf cafe house mansion.`,
-    `Crossroads Rainbow Neighbours Dallas Eastenders Pipkins Sesame Street Topcat Playschool.  The Big Match Sons and Daughters Home and Away Henry's Cat`,
+    `Crossroads Rainbow Neighbours Dallas Eastenders Pipkins Sesame Street Topcat Playschool.  The Big Match Sons and Daughters Home and Away Henry's Cat.`,
     `Metre Kilogram Second Ampere Kelvin Mole Candela.  Hertz Pascal Newton Joule Watt Henry Tesla.`,
     `Physics Chemistry Maths English French History Geography Economics Computer Studies Games RE Algebra Statistics.  C Fortran Prolog Pascal GLIM Minitab Genstat.`,
     `Ruth Langsford Anna Richardson Kathy Lette Kate Garraway Kay Burley Kimberley Guilfoyle Joanna Gosling Susanna Reid Marlene Lufen Rosana Franco Victoria Beckham.  Daphne Pink Roxette.`
 ];
+
+const form = document.querySelector('.lorem-form');
+const amount = document.getElementById("amount");
+const result = document.querySelector('.lorem-text');
+
+form.addEventListener("submit",function(e) {
+    e.preventDefault();
+    const value = parseInt(amount.value);
+});
